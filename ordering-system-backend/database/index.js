a0927841877b35451b2906f1d48bce6fffea4297
杜晓浩 <1923911906@qq.com>
2023-10-30 20:34:14 +0800

    fix: 完善github action; 修正数据库连接

--- ordering-system-backend/database/index.js
+++ ordering-system-backend/database/index.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql2");
 
-// const db = mysql.createPool({
-//   host: "ordering-system-mysql",
-//   user: "root",
-//   password: "187139",
-//   database: "ordering-system",
-// });
 const db = mysql.createPool({
-  host: "127.0.0.1",
+  host: "ordering-system-mysql",
   user: "root",
   password: "187139",
-  database: "backend",
+  database: "ordering-system",
 });
+// const db = mysql.createPool({
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "187139",
+//   database: "backend",
+// });
 
 module.exports = db;
