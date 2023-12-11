326c383d008eb778119ac6213839516aa5ada994
杜晓浩 <1923911906@qq.com>
2023-12-11 11:30:35 +0800

    deploy: refactor

--- ordering-system-backend/database/index.js
+++ ordering-system-backend/database/index.js
@@ -1,19 +1,22 @@
 const mysql = require("mysql2");
 
-let db;
-if (process.env.NODE_ENV === "production") {
-  db = mysql.createPool({
-    host: "ordering-system-mysql",
-    user: "root",
-    password: "187139",
-    database: "ordering-system",
-  });
-} else {
-  db = mysql.createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "187139",
-    database: "backend",
-  });
-}
+const DBBuilder = () => {
+  if (process.env.NODE_ENV === "production") {
+    return mysql.createPool({
+      host: "ordering-system-mysql",
+      user: "root",
+      password: "187139",
+      database: "ordering-system",
+    });
+  } else {
+    return mysql.createPool({
+      host: "127.0.0.1",
+      user: "root",
+      password: "187139",
+      database: "backend",
+    });
+  }
+};
+const db = DBBuilder();
+
 module.exports = db;
--- ordering-system-backend/ecosystem.config.js
+++ ordering-system-backend/ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "OrderingSystem", // app name
+      name: "ordering-system-backend", // app name
       script: "./app.js", // 启动执行文件
       watch: true,
-      exec_mode: "cluster",
-      instances: "max",
+      instances: "1",
+      exec_mode: "fork",
       watch_delay: 1000, // 文件变化后，延迟重启时间
       ignore_watch: ["node_modules"], // 监听忽略路径
     },
