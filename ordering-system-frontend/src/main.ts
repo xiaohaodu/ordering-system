c8caeec2acc6a9a669ef749eae63ac56a0eed87c
杜晓浩 <1923911906@qq.com>
2023-12-11 16:45:37 +0800

    fix: style

--- ordering-system-frontend/src/main.ts
+++ ordering-system-frontend/src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
 import "@/style/index.css";
 import "element-plus/theme-chalk/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import App from "./App.vue";
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
