449b877078ba426364033f6322bdeefacb1fe208
duxiaohao <du_xiaohao@foxmail.com>
2024-09-18 00:05:16 +0800

    fix

--- ordering-system-frontend/src/network/base.ts
+++ ordering-system-frontend/src/network/base.ts
@@ -2,7 +2,9 @@ const base = {
   // url: "https://os.api.mayuan.work",
   // url: "http://127.0.0.1:9000",
   url:
-    process.env.NODE_ENV === "production" ? "/api" : "https://127.0.0.1:9000",
+    process.env.NODE_ENV === "production"
+      ? "https://os.mayuan.work/api"
+      : "https://127.0.0.1:9000",
   frontendUrl:
     process.env.NODE_ENV === "production"
       ? "https://os.mayuan.work/"
